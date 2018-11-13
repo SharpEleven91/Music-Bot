590a92b879ecafe6ef0f6dd69ae4912804097c5d
Cody Biller <sharpeleven91@gmail.com>
2018-11-13 05:49:45 -0500

    finish touches on playlist and cover boundary cases

--- Bot.js
+++ Bot.js
@@ -1,36 +1,10 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
-const YTDL = require("ytdl-core");
-const YTSearch = require('youtube-search-promise');
 const Music = require('./modules/Music.js');
 const commands = config.commands;
 const Bot = new Discord.Client();
 const PREFIX = config.prefix;
-const API_KEY = config.api_key;
-const fs = require('fs');
-let queue = [];
-let nowPlaying = false;
 const Radio = new Music();
-function play(connection, message) {
-    nowPlaying = connection.playStream(YTDL(queue[0],{filter: 'audioonly', quality: "highest"}));
-    YTDL.getBasicInfo(queue[0], (err, response) => {
-        if (err) { return console.log(err) }
-        console.log(response.player_response.videoDetails);
-        let requestDisplay = response.player_response.videoDetails.title;
-        let richMessage = new Discord.RichEmbed().setTitle("Now Playing: " + requestDisplay).setColor(0xFF0000);
-        message.channel.send(richMessage);
-    })
-    queue.shift();
-
-    nowPlaying.on("end", () => {
-        if (queue.length >= 1) {
-            play(connection, message);
-        } else {
-            console.log('disconnecting');
-            connection.disconnect();
-        }
-    })
-}
 
 Bot.login(config.token);
 Bot.on("message", (message) => {
@@ -43,50 +17,9 @@ Bot.on("message", (message) => {
     let args = message.content.split(' ').slice(1, message.content.length);
     if (command === 'play') {
         Radio.Add(message, args);
-/**        let song;       
-        if (args.length <= 0) {
-            let richMessage = new Discord.RichEmbed().setTitle('Try again with a link').setColor(0xFF0000);
-            return message.channel.send(richMessage);
-        }
-        if (!YTDL.validateURL(args[0])) {
-            const opts = { maxResults: 1, key: API_KEY }
-            YTSearch(args.join(' '), opts)
-            .then(results => {
-                return results;
-            }).catch(error => {
-                console.log(error);
-            }).then(song => {
-                queue.push(song[0].link);
-                if (!message.guild.voiceConnection) {
-                    message.member.voiceChannel.join().then((connection) => {
-                        play(connection, message);
-                    }).catch((error) => console.log(error));
-                } else {
-                    YTDL.getBasicInfo(song[0].link, (err, response) => {
-                        if (err) { return message.channel.send('Song is Not Available') }
-                        let requestDisplay = response.player_response.videoDetails.title;
-                        let richMessage = new Discord.RichEmbed().setTitle(requestDisplay + " added to playlist").setColor(0xFF0000);
-                        message.channel.send(richMessage);
-                    });
-                }
-            })
-        } else {
-            queue.push(args[0]) 
-            if (!message.guild.voiceConnection) {
-                message.member.voiceChannel.join().then((connection) => {
-                    play(connection, message);
-                }).catch((error) => console.log(error));
-            }
-        } **/
     } else if (command === 'skip') {
-        Radio.Skip();
-/**        if (nowPlaying) {
-            let richMessage = new Discord.RichEmbed().setTitle("Song Skipped").setColor(0xFF0000);
-            message.channel.send(richMessage);
-            nowPlaying.end();
-        } else {
-            let richMessage = new Discord.RichEmbed().setTitle("Nothing to Skip").setColor(0xFF0000);
-            message.channel.send(richMessage);
-        } **/
+        Radio.Skip(message);
+    } else if (command === 'playlist') {
+        Radio.ShowQueue(message);
     }
 })
--- modules/Music.js
+++ modules/Music.js
@@ -2,15 +2,15 @@ const Discord = require("discord.js");
 const config = require("../config.json");
 const YTDL = require("ytdl-core");
 const YTSearch = require("youtube-search-promise");
-const commands = config.commands;
-const PREFIX = config.prefix;
 const API_KEY = config.api_key;
 module.exports = class Music {
-    constructor() {
-        this.NowPlaying = false;
-        this.Queue = [];
-    }
-    Play(connection, message) {
+  constructor() {
+    this.ActiveMessage = false;
+    this.SongList = [];
+    this.NowPlaying = false; // holds stream
+    this.Queue = []; // holds current queue of songs
+  }
+  Play(connection, message) {
     this.NowPlaying = connection.playStream(
       YTDL(this.Queue[0], { filter: "audioonly", quality: "highest" })
     );
@@ -22,25 +22,45 @@ module.exports = class Music {
       let richMessage = new Discord.RichEmbed()
         .setTitle("Now Playing: " + requestDisplay)
         .setColor(0xff0000);
-      message.channel.send(richMessage);
+      message.channel.send(richMessage).then(delete_message => { delete_message.delete(response.player_response.videoDetails.lengthSeconds * 1000) });
     });
+    this.SongList.shift();
     this.Queue.shift();
     this.NowPlaying.on("end", () => {
       if (this.Queue.length >= 1) {
+        this.NowPlaying.destroy();
         this.Play(connection, message);
       } else {
-        console.log("disconnecting");
+        this.NowPlaying.destroy();
         connection.disconnect();
       }
     });
   }
+  // shows the current queue
+  ShowQueue(message) {
+    if (this.Queue.length <= 0) {
+        let richMessage = new Discord.RichEmbed().setTitle("Playlist is currently Empty");
+        return message.channel.send(richMessage);
+    }
+    let richMessage = new Discord.RichEmbed().setTitle("Playlist").setDescription(this.SongList);
+    message.channel.send(richMessage);
+  }
+
+  CreatePlayList() {
+    return null;
+  }
+
+  DeletePlayList() {
+    return null 
+  }
+
   Skip(message) {
     if (this.NowPlaying) {
       let richMessage = new Discord.RichEmbed()
         .setTitle("Song Skipped")
         .setColor(0xff0000);
       message.channel.send(richMessage);
-      this.NowPlaying.end();
+      this.NowPlaying.end('skipped');
     } else {
       let richMessage = new Discord.RichEmbed()
         .setTitle("Nothing to Skip")
@@ -67,6 +87,12 @@ module.exports = class Music {
         .then(song => {
           this.Queue.push(song[0].link);
           if (!message.guild.voiceConnection) {
+            if (!message.member.voiceChannel) {
+                let richMessage = new Discord.RichEmbed()
+                .setTitle("You have to be in a voice channel to request a song")
+                .setColor(0xff0000);
+                return message.channel.send(richMessage)
+            }
             message.member.voiceChannel
               .join()
               .then(connection => {
@@ -79,6 +105,7 @@ module.exports = class Music {
                 return message.channel.send("Song is Not Available");
               }
               let requestDisplay = response.player_response.videoDetails.title;
+              this.SongList.push(requestDisplay);
               let richMessage = new Discord.RichEmbed()
                 .setTitle(requestDisplay + " added to playlist")
                 .setColor(0xff0000);
@@ -98,4 +125,4 @@ module.exports = class Music {
       }
     }
   }
-}
+};
